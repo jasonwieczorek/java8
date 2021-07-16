3df7b23c79dce88795df8ab0735d49c777d4b1ba
jasonwieczorek <wieczorek.jason@gmail.com>
2021-07-15 23:38:02 -0500

    added ability to build a docker image via gradle task

---
+++ src/main/java/j/m/w/jdbc/Test.java
@@ -0,0 +1,13 @@
+package j.m.w.jdbc;
+
+/**
+ * Some javadocs
+ */
+public class Test {
+
+    public static void main(String args[]) {
+
+        System.out.println("Im a java application!");
+    }
+
+}
