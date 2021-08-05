1585d36bfc4e1a560fc0fe7a11b234b6eef68596
jasonwieczorek <wieczorek.jason@gmail.com>
2021-08-04 22:20:44 -0500

    Modified DockerFile to pull an oracle DB and copy a java executable jar to the docker Image

--- src/main/java/j/m/w/jdbc/Test.java
+++ src/main/java/j/m/w/jdbc/Test.java
@@ -5,9 +5,10 @@ package j.m.w.jdbc;
  */
 public class Test {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
 
-        System.out.println("Im a java application!");
+        System.out.println("Im a java application! I will auto destruct in 1 minute");
+        Thread.sleep(60000);
     }
 
 }
